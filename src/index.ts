d576a1da71ea1d007c1293fd49fa42f76ae2955b
Aytac Malkoc <grand__ekinoks35@hotmail.com>
2022-07-13 18:22:51 +0300

    Format index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,30 @@ import { useState, useEffect } from 'react';
 import * as CryptoJS from 'crypto-js';
 
 const useStorage = (key: string, encrypt: boolean = false) => {
-	const [value, setValue] = useState(() => {
-		const json = localStorage.getItem(key);
+  const [value, setValue] = useState(() => {
+    const json = localStorage.getItem(key);
 
-		if (json) {
-			if (encrypt) {
-				const encrypted = CryptoJS.AES.decrypt(json, key).toString(CryptoJS.enc.Utf8);
-				return JSON.parse(encrypted);
-			}
-			return JSON.parse(json);
-		}
+    if (json) {
+      if (encrypt) {
+        const encrypted = CryptoJS.AES.decrypt(json, key).toString(
+          CryptoJS.enc.Utf8
+        );
+        return JSON.parse(encrypted);
+      }
+      return JSON.parse(json);
+    }
 
-		return null;
-	});
+    return null;
+  });
 
-	useEffect(() => {
-		let jsonValue = encrypt ? CryptoJS.AES.encrypt(JSON.stringify(value), key).toString() : JSON.stringify(value);
-		localStorage.setItem(key, jsonValue);
-	}, [value]);
+  useEffect(() => {
+    let jsonValue = encrypt
+      ? CryptoJS.AES.encrypt(JSON.stringify(value), key).toString()
+      : JSON.stringify(value);
+    localStorage.setItem(key, jsonValue);
+  }, [value]);
 
-	return [value, setValue];
-}
+  return [value, setValue];
+};
 
 export { useStorage };
